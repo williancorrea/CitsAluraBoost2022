6ba43fd90bac303c8b5334a0a468c4b3a82cb6f5
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2022-10-26 10:58:56 -0300

    Adicionando mock para o BD

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+
+-- CLIENTES
+INSERT INTO CLIENTES (ID, BAIRRO, CIDADE, COMPLEMENTO, CPF, ESTADO, NOME, NUMERO, RUA, TELEFONE) VALUES (1, 'Bairro', 'Cidade', 'Complemento', 'CPF', 'Estado', 'Nome', 'Numero', 'Rua', 'Telefone');
