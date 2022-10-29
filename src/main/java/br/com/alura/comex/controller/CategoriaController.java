ecda256af12c95a3ef4c1177978b39c364c0d4d5
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2022-10-29 18:19:46 -0300

    Adicionando controller de categorias

---
+++ src/main/java/br/com/alura/comex/controller/CategoriaController.java
@@ -0,0 +1,36 @@
+package br.com.alura.comex.controller;
+
+import br.com.alura.comex.model.Categoria;
+import br.com.alura.comex.model.Cliente;
+import br.com.alura.comex.model.dto.CategoriaDto;
+import br.com.alura.comex.model.dto.CategoriaDto;
+import br.com.alura.comex.service.CategoriaService;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/categorias")
+public class CategoriaController {
+
+    private final CategoriaService categoriaService;
+
+    public CategoriaController(@Lazy CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoriaDto> cadastrar(@RequestBody @Valid CategoriaDto clienteDto, UriComponentsBuilder uriBuilder) {
+        Categoria categoria = categoriaService.inserir(clienteDto);
+        URI uri = uriBuilder.path("/categorias/{id}").buildAndExpand(categoria.getId()).toUri();
+        return ResponseEntity.created(uri).body(new CategoriaDto(categoria));
+    }
+}
