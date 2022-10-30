bf9e99881e3275be6fd77af0a951d6936a73d21f
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2022-10-30 14:42:05 -0300

    Lista paginada de produtos

--- src/main/java/br/com/alura/comex/controller/ProdutoController.java
+++ src/main/java/br/com/alura/comex/controller/ProdutoController.java
@@ -1,9 +1,14 @@
 package br.com.alura.comex.controller;
 
 import br.com.alura.comex.model.Produto;
+import br.com.alura.comex.model.dto.ClienteDto;
 import br.com.alura.comex.model.dto.ProdutoDto;
 import br.com.alura.comex.service.ProdutoService;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +18,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -22,6 +29,15 @@ public class ProdutoController {
 
     public ProdutoController(@Lazy ProdutoService produtoService) {
         this.produtoService = produtoService;
+    }
+
+    @RequestMapping
+    public List<ProdutoDto> listar( @PageableDefault(sort = {"nome"}, direction = Sort.Direction.ASC, value = 5) Pageable pageable) {
+        return produtoService.listarTodos(pageable)
+                .stream().toList()
+                .parallelStream()
+                .map(ProdutoDto::new)
+                .collect(Collectors.toList());
     }
 
     @PostMapping
