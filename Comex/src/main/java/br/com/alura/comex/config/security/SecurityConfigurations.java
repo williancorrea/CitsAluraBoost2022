07c9b06c6b9e000fc425c4a736cb3a86015d1942
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2022-11-06 16:28:25 -0300

    Atualizando Swager para OpenApi

--- Comex/src/main/java/br/com/alura/comex/config/security/SecurityConfigurations.java
+++ Comex/src/main/java/br/com/alura/comex/config/security/SecurityConfigurations.java
@@ -1,74 +1,66 @@
 package br.com.alura.comex.config.security;
 
+import br.com.alura.comex.filter.AutenticacaoViaTokenFilter;
+import br.com.alura.comex.service.AutenticacaoService;
+import br.com.alura.comex.service.TokenService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity
 @Configuration
-public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfigurations {
 
+    private final AuthenticationConfiguration configuration;
     private final AutenticacaoService autenticacaoService;
     private final TokenService tokenService;
 
-    public SecurityConfigurations(@Lazy AutenticacaoService autenticacaoService,
-                                  @Lazy TokenService tokenService) {
+    public SecurityConfigurations(AuthenticationConfiguration configuration,
+                                  AutenticacaoService autenticacaoService,
+                                  TokenService tokenService) {
         this.autenticacaoService = autenticacaoService;
         this.tokenService = tokenService;
-    }
-
-    /**
-     * Configuracoes de autenticacao
-     *
-     * @param auth
-     * @throws Exception
-     */
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
-    }
-
-    /**
-     * Configuracoes de autorizacao
-     *
-     * @param http
-     * @throws Exception
-     */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/auth").permitAll()
-
-                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/categorias").permitAll()
-
-                .anyRequest()
-                .authenticated()
-                .and()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, autenticacaoService), UsernamePasswordAuthenticationFilter.class);
+        this.configuration = configuration;
     }
 
     @Bean
-    @Override
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
+    public AuthenticationManager authenticationManager() throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
-        super.configure(web);
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/categorias", "/api/produtos").permitAll()
+
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(
+                        new AutenticacaoViaTokenFilter(tokenService, autenticacaoService),
+                        UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
+
+
---
+++ Comex/src/main/java/br/com/alura/comex/config/swagger/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package br.com.alura.comex.config.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "API for Alura Courses", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer")
+public class OpenApiConfig {
+}
